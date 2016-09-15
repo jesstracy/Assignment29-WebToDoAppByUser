6d37bc6707ff655c7d10bd518e244a9b0df90896
Jessica Tracy <j.tracy916@gmail.com>
2016-09-15 15:11:17 -0400

    Made ToDo entity

---
+++ src/main/java/com/tiy/ToDo.java
@@ -0,0 +1,28 @@
+package com.tiy;
+
+import javax.persistence.*;
+
+/**
+ * Created by jessicatracy on 9/15/16.
+ */
+@Entity
+@Table(name = "todos")
+public class ToDo {
+    @Id
+    @GeneratedValue
+    int id;
+
+    @Column(nullable = false)
+    String text;
+
+    @Column(nullable = false)
+    boolean isDone;
+
+    public ToDo() {
+    }
+
+    public ToDo(String text) {
+        this.text = text;
+        this.isDone = false;
+    }
+}
