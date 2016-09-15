a4b95d2ba45864321ea57e22217b4fd11bd38206
Jessica Tracy <j.tracy916@gmail.com>
2016-09-15 18:15:46 -0400

    First unit test with Spring

--- src/main/java/com/tiy/ToDoRepository.java
+++ src/main/java/com/tiy/ToDoRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 /**
  * Created by jessicatracy on 9/15/16.
  */
-public interface ToDoRepository extends CrudRepository<ToDo, Integer>{
+public interface ToDoRepository extends CrudRepository<ToDo, Integer> {
+    ToDo findFirstByText(String text);
 }
--- src/test/java/com/tiy/Assignment29ApplicationTests.java
+++ src/test/java/com/tiy/Assignment29ApplicationTests.java
@@ -2,15 +2,40 @@ package com.tiy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Assignment29ApplicationTests {
 
+	@Autowired
+	ToDoRepository todos;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testInsertToDo() {
+		System.out.println("testInsertToDo()");
+		System.out.println("my todos Repository = " + todos.toString());
+
+		String testToDoText = "Test ToDo";
+
+		ToDo testToDo = new ToDo(testToDoText);
+		todos.save(testToDo);
+
+		ToDo retrievedToDo = todos.findFirstByText(testToDoText);
+		assertNotNull(retrievedToDo);
+
+		todos.delete(testToDo);
+		retrievedToDo = todos.findFirstByText(testToDoText);
+		assertNull(retrievedToDo);
 	}
 
 }
