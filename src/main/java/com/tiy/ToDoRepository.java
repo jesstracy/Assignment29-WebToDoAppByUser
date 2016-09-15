0c9ddaa200bc147a1637afbae3ff0f87f9921a85
Jessica Tracy <j.tracy916@gmail.com>
2016-09-15 16:01:11 -0400

    Now can display all todos (no login yet)

---
+++ src/main/java/com/tiy/ToDoRepository.java
@@ -0,0 +1,9 @@
+package com.tiy;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jessicatracy on 9/15/16.
+ */
+public interface ToDoRepository extends CrudRepository<ToDo, Integer>{
+}
--- src/main/java/com/tiy/ToDoWebAppController.java
+++ src/main/java/com/tiy/ToDoWebAppController.java
@@ -1,16 +1,36 @@
 package com.tiy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
 
 /**
  * Created by jessicatracy on 9/15/16.
  */
 @Controller
 public class ToDoWebAppController {
-    @RequestMapping(path="/", method= RequestMethod.GET)
-    public String home() {
+    @Autowired
+    ToDoRepository todos;
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String home(Model model) {
+        Iterable<ToDo> listOfTodosIterable = todos.findAll();
+        ArrayList<ToDo> listOfTodos = new ArrayList<>();
+        for (ToDo todo : listOfTodosIterable) {
+            listOfTodos.add(todo);
+        }
+        model.addAttribute("toDoItems", listOfTodos);
         return "home";
+    }
+
+    @RequestMapping(path = "/add-todo", method = RequestMethod.POST)
+    public String addToDo(String todoText) {
+        ToDo todo = new ToDo(todoText);
+        todos.save(todo);
+        return "redirect:/";
     }
 }
